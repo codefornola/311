01df1c69025b0a9a94c3693ff20d2117d09d3d8f
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-11-02 15:01:16 -0500

    Cleaned up formatting in setup scripts and view script.

--- setup/sanitize.sql
+++ setup/sanitize.sql
@@ -2,21 +2,22 @@ alter role nola311 set search_path to nola311, public;
 create extension if not exists postgis;
 
 create table nola311.calls as (
-  select id,
-         ticket_id,
-         issue_type,
-         to_timestamp(ticket_created_date_time,'MM/DD/YYYY HH12:MI:SS AM') as ticket_created_date_time,
-         to_timestamp(ticket_closed_date_time,'MM/DD/YYYY HH12:MI:SS AM') as ticket_closed_date_time,
-         ticket_status,
-         issue_description,
-         street_address,
-         neighborhood_district,
-         council_district,
-         city,
-         state,
-         zip_code,
-         location,
-         st_pointfromtext('POINT(' || longitude || ' ' || latitude || ')', 4326) as geom
+  select
+    id,
+    ticket_id,
+    issue_type,
+    to_timestamp(ticket_created_date_time,'MM/DD/YYYY HH12:MI:SS AM') as ticket_created_date_time,
+    to_timestamp(ticket_closed_date_time,'MM/DD/YYYY HH12:MI:SS AM') as ticket_closed_date_time,
+    ticket_status,
+    issue_description,
+    street_address,
+    neighborhood_district,
+    council_district,
+    city,
+    state,
+    zip_code,
+    location,
+    st_pointfromtext('POINT(' || longitude || ' ' || latitude || ')', 4326) as geom
   from nola311.calls_tmp
 );
 
