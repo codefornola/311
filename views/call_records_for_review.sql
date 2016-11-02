ecd39320d8516906d54f982a6bada080cba83270
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2016-11-02 15:01:16 -0500

    Cleaned up formatting in setup scripts and view script.

--- views/call_records_for_review.sql
+++ views/call_records_for_review.sql
@@ -3,21 +3,21 @@ set search_path to 'nola311';
 drop view if exists nola311.call_records_for_review;
 create view nola311.call_records_for_review as (
   select
-    id
-    ,ticket_id
-    ,issue_type
-    ,ticket_created_date_time
-    ,ticket_closed_date_time
-    ,ticket_status
-    ,issue_description
-    ,street_address
-    ,neighborhood_district
-    ,council_district
-    ,city
-    ,state
-    ,zip_code
-    ,location
-    ,geom
+    id,
+    ticket_id,
+    issue_type,
+    ticket_created_date_time,
+    ticket_closed_date_time,
+    ticket_status,
+    issue_description,
+    street_address,
+    neighborhood_district,
+    council_district,
+    city,
+    state,
+    zip_code,
+    location,
+    geom
   from nola311.calls
   where
   	state is null
