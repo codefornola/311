01ff5ed42a29d4aad7458c291945013550178775
Marc Cenac <marc.j.cenac@gmail.com>
2017-01-01 10:37:28 -0600

    adding initial rest api

---
+++ app/queries.js
@@ -0,0 +1,38 @@
+var promise = require('bluebird');
+
+var options = {
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var dbUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/nola311';
+var db = pgp(dbUrl);
+
+function findAllCalls(req, res, next) {
+  let pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 50;
+
+  db.any('select * from nola311.calls limit $1', pageSize)
+    .then(function (data) {
+      res.status(200).json({ status: 'success', data: data });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function findCallsByTicketId(req, res, next) {
+  let ticketId = req.params.ticketId;
+
+  db.one('select * from nola311.calls where ticket_id=$1', ticketId)
+    .then(function (data) {
+      res.status(200).json({ status: 'success', data: data });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+    findAllCalls: findAllCalls,
+    findCallsByTicketId: findCallsByTicketId,
+};
