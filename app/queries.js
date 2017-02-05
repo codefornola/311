d85f2da091430b34a990a3da061b639d533981a4
Marc Cenac <marc.j.cenac@gmail.com>
2017-02-05 09:29:29 -0600

    plot chart for issue types

--- app/queries.js
+++ app/queries.js
@@ -32,7 +32,19 @@ function findCallsByTicketId(req, res, next) {
     });
 }
 
+function findCallTypeTotals(req, res, next) {
+  db.any('with totals as (select count(*) as total, issue_type from nola311.calls group by issue_type order by total desc) select issue_type, total from totals')
+    .then(function (data) {
+      console.log(data);
+      res.status(200).json({ status: 'success', data: data });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
 module.exports = {
     findAllCalls: findAllCalls,
     findCallsByTicketId: findCallsByTicketId,
+    findCallTypeTotals: findCallTypeTotals,
 };
