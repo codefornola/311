6b4216323199f02abaa28bfe20e862e26ecd0925
Ryan B. Harvey <ryan.b.harvey@ted.com>
2017-09-23 13:53:15 -0500

    Updated db setup scripts to use env vars for host, user and db name.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,21 @@
 #!/usr/bin/bash
 #!/usr/local/bin/bash
 
-echo ""
-echo "Creating nola311 user and database"
-echo ""
-createuser nola311
-createdb nola311 -O nola311
+NOLA311_DB_USER=${NOLA311_DB_USER:-postgres}
+NOLA311_DB_HOST=${NOLA311_DB_HOST:-localhost}
+NOLA311_DB_NAME=${NOLA311_DB_NAME:-nola311}
+
+if [ $NOLA311_DB_HOST == "localhost" ]; then
+  echo ""
+  echo "Creating nola311 user and database"
+  echo ""
+  createuser nola311
+  createdb nola311 -O nola311
+else
+  echo ""
+  echo "Skipping creation of user and database: not on localhost"
+  echo ""
+fi
 
 echo ""
 echo "Downloading the source data"
@@ -14,7 +24,7 @@ echo ""
 ./setup/download_source_data.sh
 
 echo ""
-echo "Setup tables and load data into database" 
+echo "Setup tables and load data into database"
 echo ""
 ./setup/load_data.sh
 
