f02836b34cc97f392fb460d8556153e3560d3a2f
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2017-09-23 14:38:29 -0500

    Database updates to support deploy to RDS. (#26)
    * Updated db setup scripts to use env vars for host, user and db name.
    
    * Updated README with new env var names.
    
    * Added env var to specify database port.

--- setup/load_data.sh
+++ setup/load_data.sh
@@ -3,28 +3,29 @@
 #!/usr/local/bin/bash
 set -e
 
-DB_USER=${DB_USER:-postgres}
-DB_HOST=${DB_HOST:-localhost}
-DB_NAME=${DB_NAME:-nola311}
+NOLA311_DB_USER=${NOLA311_DB_USER:-postgres}
+NOLA311_DB_HOST=${NOLA311_DB_HOST:-localhost}
+NOLA311_DB_PORT=${NOLA311_DB_PORT:-5432}
+NOLA311_DB_NAME=${NOLA311_DB_NAME:-nola311}
 data_dir=$(pwd)/data
 neighborhood_areas_file=$data_dir/neighborhood_areas.geo.json
 neighborhood_areas_file=$data_dir/neighborhood_areas.geo.json
 call_data_file=$data_dir/nola311_raw.csv
 
 echo ""
-echo "Setting up schema to load data into $DB_NAME on $DB_HOST"
+echo "Setting up schema to load data into $NOLA311_DB_NAME on $NOLA311_DB_HOST:$NOLA311_DB_PORT"
 echo ""
-psql -U $DB_USER -d $DB_NAME -h $DB_HOST -f setup/setup_schema.sql
+psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f setup/setup_schema.sql
 
 echo ""
 echo "Loading data from $call_data_file"
 echo "This may take a minute..."
 echo ""
-psql -U $DB_USER -d $DB_NAME -h $DB_HOST \
+psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT \
      -c "\copy nola311.calls_tmp(ticket_id,issue_type,ticket_created_date_time,ticket_closed_date_time,ticket_status,issue_description,street_address,neighborhood_district,council_district,city,state,zip_code,location,geom,latitude,longitude) from '$call_data_file' with csv header NULL as '';"
 
 echo ""
 echo "Loading data from $neighborhood_areas_file"
 echo ""
-psql -U $DB_USER -d $DB_NAME -h $DB_HOST \
+psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT \
      -c "\copy nola311.neighborhood_areas_tmp(json_data) from '$neighborhood_areas_file' csv quote e'\x01' delimiter e'\x02';"
