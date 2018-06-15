db2971bc89ec42b0416d023eab426a02576e3a74
Ryan B. Harvey <ryan.b.harvey@ted.com>
2018-06-14 21:12:30 -0500

    Modify db loading scripts to use DB URI instead of separate CLI args.

--- setup/install.sh
+++ setup/install.sh
@@ -3,11 +3,11 @@
 #!/usr/local/bin/bash
 
 # sanitize the tables
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f setup/sanitize_call_data.sql
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f setup/sanitize_neighborhood_data.sql
+psql $NOLA311_DB_URI -f setup/sanitize_call_data.sql
+psql $NOLA311_DB_URI -f setup/sanitize_neighborhood_data.sql
 
 # create views
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f views/open_tickets_stats.sql -q
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f views/closed_tickets_stats.sql -q
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f views/call_records_for_review.sql -q
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f views/call_records_with_call_for_details.sql -q
+psql $NOLA311_DB_URI -f views/open_tickets_stats.sql -q
+psql $NOLA311_DB_URI -f views/closed_tickets_stats.sql -q
+psql $NOLA311_DB_URI -f views/call_records_for_review.sql -q
+psql $NOLA311_DB_URI -f views/call_records_with_call_for_details.sql -q
--- setup/load_data.sh
+++ setup/load_data.sh
@@ -11,17 +11,17 @@ call_data_file=$data_dir/nola311_raw.csv
 echo ""
 echo "Setting up schema to load data into $NOLA311_DB_NAME on $NOLA311_DB_HOST:$NOLA311_DB_PORT"
 echo ""
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT -f setup/setup_schema.sql
+psql $NOLA311_DB_URI -f setup/setup_schema.sql
 
 echo ""
 echo "Loading data from $call_data_file"
 echo "This may take a minute..."
 echo ""
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT \
+psql $NOLA311_DB_URI \
      -c "\copy nola311.calls_tmp(ticket_id,issue_type,ticket_created_date_time,ticket_closed_date_time,ticket_status,issue_description,street_address,neighborhood_district,council_district,city,state,zip_code,location,geom,latitude,longitude) from '$call_data_file' with csv header NULL as '';"
 
 echo ""
 echo "Loading data from $neighborhood_areas_file"
 echo ""
-psql -U $NOLA311_DB_USER -d $NOLA311_DB_NAME -h $NOLA311_DB_HOST -p $NOLA311_DB_PORT \
+psql $NOLA311_DB_URI \
      -c "\copy nola311.neighborhood_areas_tmp(json_data) from '$neighborhood_areas_file' csv quote e'\x01' delimiter e'\x02';"
